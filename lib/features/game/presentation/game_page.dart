5b335fe4d092b8fbacfcb2c47ff45b67ce4401ea
Nabil Akhunjee <nbakh16@gmail.com>
2025-08-20 16:51:49 +0600

    added color green/red, win/lose image

--- lib/features/game/presentation/game_page.dart
+++ lib/features/game/presentation/game_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hangman_game/core/core.dart';
 
 import '../game.dart';
 
@@ -8,13 +9,19 @@ class GamePage extends StatelessWidget {
   const GamePage({super.key});
 
   // Temporary dialog, will change later
-  void _showEndGameDialog(BuildContext context, String title, String word) {
+  void _showEndGameDialog(BuildContext context, bool isWon, String word) {
     showDialog(
       context: context,
       barrierDismissible: false,
       builder: (dialogContext) {
         return AlertDialog(
-          title: Text(title),
+          // title: Text(title),
+          title: Image.asset(
+            isWon ? 'assets/hangman/win.png' : 'assets/hangman/lose.png',
+            height: MediaQuery.of(context).size.height * 0.33,
+            fit: BoxFit.contain,
+            color: Colors.white,
+          ),
           content: Text('The word was: $word'),
           actions: [
             TextButton(
@@ -35,9 +42,9 @@ class GamePage extends StatelessWidget {
     return BlocListener<GameCubit, GameState>(
       listener: (context, state) {
         if (state is GameWon) {
-          _showEndGameDialog(context, 'You Won!', state.word);
+          _showEndGameDialog(context, true, state.word);
         } else if (state is GameLost) {
-          _showEndGameDialog(context, 'You Lost!', state.word);
+          _showEndGameDialog(context, false, state.word);
         }
       },
       child: Scaffold(
@@ -46,25 +53,40 @@ class GamePage extends StatelessWidget {
             padding: const EdgeInsets.symmetric(horizontal: 16),
             child: Container(
               alignment: Alignment.center,
-              child: BlocBuilder<GameCubit, GameState>(
+              child: // Inside your GameView's build method
+              BlocBuilder<GameCubit, GameState>(
                 builder: (context, state) {
-                  if (state is GameInitial) {
-                    return const Center(child: CircularProgressIndicator());
-                  }
+                  switch (state) {
+                    case GameInitial():
+                      return const Center(child: CircularProgressIndicator());
 
-                  if (state is GamePlaying) {
-                    return _buildGameUI(context, state.gameModel.lives, state.maskedWord, state.guessedLetters, true);
-                  }
+                    case final GamePlaying s:
+                      return _buildGameUI(
+                        context,
+                        lives: s.gameModel.lives,
+                        word: s.maskedWord,
+                        guessedLetters: s.guessedLetters,
+                        isPlaying: true,
+                      );
 
-                  if (state is GameWon) {
-                    return _buildGameUI(context, 6, state.word, state.guessedLetters, false);
-                  }
+                    case final GameWon s:
+                      return _buildGameUI(
+                        context,
+                        lives: 6,
+                        word: s.word,
+                        guessedLetters: s.guessedLetters,
+                        isPlaying: false,
+                      );
 
-                  if (state is GameLost) {
-                    return _buildGameUI(context, 0, state.word, state.guessedLetters, false);
+                    case final GameLost s:
+                      return _buildGameUI(
+                        context,
+                        lives: 0,
+                        word: s.word,
+                        guessedLetters: s.guessedLetters,
+                        isPlaying: false,
+                      );
                   }
-
-                  return const Center(child: Text('Something went wrong!'));
                 },
               ),
             ),
@@ -74,7 +96,13 @@ class GamePage extends StatelessWidget {
     );
   }
 
-  Widget _buildGameUI(BuildContext context, int lives, String word, Set<String> guessedLetters, bool isPlaying) {
+  Widget _buildGameUI(
+    BuildContext context, {
+    required int lives,
+    required String word,
+    required Set<String> guessedLetters,
+    required bool isPlaying,
+  }) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.center,
@@ -87,7 +115,6 @@ class GamePage extends StatelessWidget {
         Expanded(
           child: Image.asset(
             'assets/hangman/${6 - lives}.png',
-            height: MediaQuery.of(context).size.height * 0.4,
             fit: BoxFit.contain,
             color: Colors.white,
           ),
@@ -115,6 +142,15 @@ class GamePage extends StatelessWidget {
             final char = String.fromCharCode('A'.codeUnitAt(0) + index);
             final isGuessed = guessedLetters.contains(char);
 
+            Color borderColor = Colors.transparent;
+            if (isGuessed) {
+              if (word.contains(char)) {
+                borderColor = AppColor.green;
+              } else {
+                borderColor = AppColor.red;
+              }
+            }
+
             return ElevatedButton(
               onPressed: (isGuessed || !isPlaying)
                   ? null // Disable button if guessed or if the game is over
@@ -122,9 +158,13 @@ class GamePage extends StatelessWidget {
                       context.read<GameCubit>().guessLetter(char);
                     },
               style: ElevatedButton.styleFrom(
-                shape: const CircleBorder(),
+                shape: CircleBorder(
+                  side: BorderSide(color: borderColor, width: 2),
+                ),
                 padding: const EdgeInsets.all(8),
-                backgroundColor: isGuessed ? Colors.grey.shade700 : null,
+                // backgroundColor: isGuessed ? Colors.grey.shade700 : null,
+                disabledBackgroundColor: Colors.grey.shade700,
+                disabledForegroundColor: Colors.white,
               ),
               child: FittedBox(child: Text(char)),
             );
